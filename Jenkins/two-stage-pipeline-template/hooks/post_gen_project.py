9ce454c1bdd52b56c6e13a708a960ff528f7c698
Sam Liu <xinhol@amazon.com>
2021-04-15 10:05:59 -0700

    Add Jenkins 2-stage pipeline template (#3)

---
+++ Jenkins/two-stage-pipeline-template/hooks/post_gen_project.py
@@ -0,0 +1,17 @@
+import os
+
+output_dir = "{{ cookiecutter.outputDir }}"
+project_root_dir = os.path.relpath(".", output_dir)
+jenkinsfile = "Jenkinsfile"
+
+# move "Jenkinsfile" to the root of the project
+destination_file_path = os.path.join(project_root_dir, jenkinsfile)
+if os.path.exists(destination_file_path):
+    raise Exception(
+        f"{jenkinsfile} already exists in project root directory. Please remove it first."
+    )
+
+os.rename(jenkinsfile, os.path.join(project_root_dir, jenkinsfile))
+
+# There is only one file in output_dir, remove it
+os.rmdir(os.path.join(project_root_dir, output_dir))
