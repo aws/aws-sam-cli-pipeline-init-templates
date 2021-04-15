c590192b26dcdcab86bbe710b72d0c163b61bb68
Sam Liu <xinhol@amazon.com>
2021-04-15 16:08:39 -0700

    Update post_gen_project.py

--- GitHub Actions/two-stage-pipeline-template/hooks/post_gen_project.py
+++ GitHub Actions/two-stage-pipeline-template/hooks/post_gen_project.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 output_dir = "{{ cookiecutter.outputDir }}"
 project_root_dir = os.path.relpath(".", output_dir)
-workflow_dir = os.path.join(project_root_dir, '.github', 'workflows')
+workflow_dir = Path(project_root_dir, ".github", "workflows")
 workflow_file_name = "pipeline.yaml"
 
 # make sure workflow directory exists
-Path(workflow_dir).mkdir(parents=True, exist_ok=True)
+workflow_dir.mkdir(parents=True, exist_ok=True)
 
 # move workflow file to the workflow directory
-destination_file_path = os.path.join(workflow_dir, workflow_file_name)
-if os.path.exists(destination_file_path):
+destination_file = Path(workflow_dir, workflow_file_name)
+if destination_file.exists():
     raise Exception(
         f"{workflow_file_name} already exists in {workflow_dir}. Please remove it first."
     )
 
-os.rename(workflow_file_name, destination_file_path)
+os.rename(workflow_file_name, destination_file)
 
 # There is only one file in output_dir, remove it
-os.rmdir(os.path.join(project_root_dir, output_dir))
+os.rmdir(Path.cwd().resolve())
