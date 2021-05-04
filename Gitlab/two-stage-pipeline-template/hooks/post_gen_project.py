6c4a1597fee3f71f336e81e21a549c5840e7d0d2
Xia Zhao <xazhao@amazon.com>
2021-05-04 13:15:46 -0700

    update template files

--- Gitlab/two-stage-pipeline-template/hooks/post_gen_project.py
+++ Gitlab/two-stage-pipeline-template/hooks/post_gen_project.py
@@ -1,14 +1,24 @@
 import os
+import shutil
 
 output_dir = "{{ cookiecutter.outputDir }}"
 project_root_dir = os.path.relpath(".", output_dir)
 gitlabFile = ".gitlab-ci.yml"
+assumeRoleFile = "assume-role.sh"
 
-# move ".gitlab-ci.yml" to the root of the project
-destination_file_path = os.path.join(project_root_dir, gitlabFile)
-if os.path.exists(destination_file_path):
+# move ".gitlab-ci.yml" and "assume-role.sh" to the root of the project
+gitlab_file_path = os.path.join(project_root_dir, gitlabFile)
+assume_role_file_path = os.path.join(project_root_dir, assumeRoleFile)
+if os.path.exists(gitlab_file_path):
     raise Exception(
         f"{gitlabFile} already exists in project root directory. Please remove it first."
     )
+if os.path.exists(assume_role_file_path):
+    raise Exception(
+        f"{assumeRoleFile} already exists in project root directory. Please remove it first."
+    )
 
 os.rename(gitlabFile, os.path.join(project_root_dir, gitlabFile))
+os.rename(assumeRoleFile, os.path.join(project_root_dir, assumeRoleFile))
+
+shutil.rmtree(os.path.join(project_root_dir, output_dir))
--- Gitlab/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
+++ Gitlab/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
@@ -1,18 +1,23 @@
+#!/bin/bash
 REGION=$1
 ROLE=$2
 SESSION_NAME=$3
 
 unset AWS_SESSION_TOKEN
 export AWS_DEFAULT_REGION=$REGION
-
-export AWS_ACCESS_KEY_ID=$(echo $AWS_ACCESS_KEY_ID_)
-export AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY_)
+export AWS_ACCESS_KEY_ID=$PIPELINE_USER_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$PIPELINE_USER_SECRET_ACCESS_KEY
 
 cred=$(aws sts assume-role --role-arn "$ROLE" \
                            --role-session-name "$SESSION_NAME" \
                            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
                            --output text)
 
-export AWS_ACCESS_KEY_ID=$(echo $cred | awk '{ print $1 }')
-export AWS_SECRET_ACCESS_KEY=$(echo $cred | awk '{ print $2 }')
-export AWS_SESSION_TOKEN=$(echo $cred | awk '{ print $3 }')
+AccessKeyId=$(echo "$cred" | awk '{ print $1 }')
+export AWS_ACCESS_KEY_ID=$AccessKeyId
+
+SecretAccessKey=$(echo "$cred" | awk '{ print $2 }')
+export AWS_SECRET_ACCESS_KEY=$SecretAccessKey
+
+SessionToken=$(echo "$cred" | awk '{ print $3 }')
+export AWS_SESSION_TOKEN=$SessionToken
