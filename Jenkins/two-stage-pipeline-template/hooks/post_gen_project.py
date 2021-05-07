a0b8934ec9caa1ba0a19979015ef43c281b493b6
Xia Zhao <78883180+xazhao@users.noreply.github.com>
2021-05-07 11:51:04 -0700

    Add GitLab 2-stage pipeline template (#7)

--- Jenkins/two-stage-pipeline-template/hooks/post_gen_project.py
+++ Jenkins/two-stage-pipeline-template/hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 output_dir = "{{ cookiecutter.outputDir }}"
 project_root_dir = os.path.relpath(".", output_dir)
@@ -14,4 +15,4 @@ if os.path.exists(destination_file_path):
 os.rename(jenkinsfile, os.path.join(project_root_dir, jenkinsfile))
 
 # There is only one file in output_dir, remove it
-os.rmdir(os.path.join(project_root_dir, output_dir))
+shutil.rmtree(os.path.join(project_root_dir, output_dir))
