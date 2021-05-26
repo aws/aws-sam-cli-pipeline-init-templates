778635d0c0a7f1210739a8bbf3d1e3149c9542a5
Ahmed Elbayaa <elbayaaa@amazon.com>
2021-05-26 13:07:27 -0700

    hotfixes

--- Gitlab/two-stage-pipeline-template/hooks/post_gen_project.py
+++ Gitlab/two-stage-pipeline-template/hooks/post_gen_project.py
@@ -7,8 +7,8 @@ gitlab_file = ".gitlab-ci.yml"
 assume_role_file = "assume-role.sh"
 
 # move ".gitlab-ci.yml" and "assume-role.sh" to the root of the project
-gitlab_file_path = os.path.join(project_root_dir, gitlabFile)
-assume_role_file_path = os.path.join(project_root_dir, assumeRoleFile)
+gitlab_file_path = os.path.join(project_root_dir, gitlab_file)
+assume_role_file_path = os.path.join(project_root_dir, assume_role_file)
 if os.path.exists(gitlab_file_path):
     raise Exception(
         f"{gitlab_file} already exists in project root directory. Please remove it first."
