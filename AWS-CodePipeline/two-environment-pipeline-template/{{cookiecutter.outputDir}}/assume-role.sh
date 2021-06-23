d7f071556ad450bf25c8506a4bf99a9ca7814386
Sam Liu <xinhol@amazon.com>
2021-06-23 10:01:09 -0700

    Remove region from assume-role

--- AWS-CodePipeline/two-environment-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
+++ AWS-CodePipeline/two-environment-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-REGION=$1
-ROLE=$2
-SESSION_NAME=$3
+ROLE=$1
+SESSION_NAME=$2
 
 # Unset AWS credentials stored in env so that every time this script runs,
 # it will use the AWS CodeBuild service role to assume the target IAM roles.
 unset AWS_SESSION_TOKEN
-export AWS_DEFAULT_REGION=$REGION
+unset AWS_DEFAULT_REGION
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 
