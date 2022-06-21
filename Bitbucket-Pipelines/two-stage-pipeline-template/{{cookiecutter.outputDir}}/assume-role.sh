88bb4e73bdc1e74c39788830cd408d266e820693
Justice Sidhu <sidhujus@amazon.com>
2022-06-21 14:35:10 -0700

    Support for OIDC

--- Bitbucket-Pipelines/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
+++ Bitbucket-Pipelines/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
@@ -2,17 +2,29 @@
 ROLE=$1
 SESSION_NAME=$2
 PROFILE_NAME=$3
+PERMISSIONS_PROVIDER=$4
+TOKEN=$5
 
 unset AWS_SESSION_TOKEN
 
-aws configure --profile sam-pipeline-user set aws_access_key_id "$PIPELINE_USER_ACCESS_KEY_ID"
-aws configure --profile sam-pipeline-user set aws_secret_access_key "$PIPELINE_USER_SECRET_ACCESS_KEY"
+if [ "$PERMISSIONS_PROVIDER" = "IAM" ]
+then
+    aws configure --profile sam-pipeline-user set aws_access_key_id "$PIPELINE_USER_ACCESS_KEY_ID"
+    aws configure --profile sam-pipeline-user set aws_secret_access_key "$PIPELINE_USER_SECRET_ACCESS_KEY"
 
-cred=$(aws sts assume-role --profile sam-pipeline-user \
-                           --role-arn "$ROLE" \
-                           --role-session-name "$SESSION_NAME" \
-                           --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
-                           --output text)
+    cred=$(aws sts assume-role --profile sam-pipeline-user \
+                            --role-arn "$ROLE" \
+                            --role-session-name "$SESSION_NAME" \
+                            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+                            --output text)
+else
+    # Use assume-role-with-web-identity instead of assume-role
+    cred=$(aws sts assume-role-with-web-identity --role-arn "$ROLE" \
+                            --role-session-name "$SESSION_NAME" \
+                            --web-identity-token "$TOKEN" \
+                            --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
+                            --output text)
+fi
 
 ACCESS_KEY_ID=$(echo "$cred" | awk '{ print $1 }')
 aws configure --profile "$PROFILE_NAME" set aws_access_key_id "$ACCESS_KEY_ID"
