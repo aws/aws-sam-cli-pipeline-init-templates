5e743b0d20c96eaf28cfaf11b3b9d1aec5befbb3
Justice Sidhu <sidhujus@amazon.com>
2022-07-13 10:28:44 -0700

    Use Jinja for if statements

--- Gitlab/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
+++ Gitlab/two-stage-pipeline-template/{{cookiecutter.outputDir}}/assume-role.sh
@@ -12,7 +12,7 @@ then
     # Use assume-role-with-web-identity instead of assume-role
     cred=$(aws sts assume-role-with-web-identity --role-arn "$ROLE" \
                             --role-session-name "$SESSION_NAME" \
-                            --web-identity-token $CI_JOB_JWT_V2 \
+                            --web-identity-token "$CI_JOB_JWT_V2" \
                             --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
                             --output text)
 
@@ -22,12 +22,12 @@ else
                         --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
                         --output text)
 fi
-    ACCESS_KEY_ID=$(echo "$cred" | awk '{ print $1 }')
-    export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
+ACCESS_KEY_ID=$(echo "$cred" | awk '{ print $1 }')
+export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
 
-    SECRET_ACCESS_KEY=$(echo "$cred" | awk '{ print $2 }')
-    export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
+SECRET_ACCESS_KEY=$(echo "$cred" | awk '{ print $2 }')
+export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
 
-    SESSION_TOKEN=$(echo "$cred" | awk '{ print $3 }')
-    export AWS_SESSION_TOKEN=$SESSION_TOKEN
+SESSION_TOKEN=$(echo "$cred" | awk '{ print $3 }')
+export AWS_SESSION_TOKEN=$SESSION_TOKEN
 
